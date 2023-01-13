7741a35244fd9d458d986be8ab844fd3312a35ff
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-01-13 13:53:48 -0500

    Add code to satisfy challenge number 2

---
+++ challenge2.js
@@ -0,0 +1,15 @@
+// 2. Start with an array of strings with a mix of uppercase and lowercase letters.
+// Print every word in all lowercase letters.
+
+// goal is print an entire string in all lowercase letters.
+// create an arry
+// create a for statement
+// create a console.log and run to test output
+// add code "toLowerCase" and see if we get desired outcome
+
+var bikes = ["motorcYcle", "MounTain", "unICycle", "RecuMbent"];
+
+for (var i = 0; i < bikes.length; i++) {
+    console.log(bikes[i].toLowerCase());
+}
+// I got hung up then googled the lowercase method and found out I needed the parenthesis.
